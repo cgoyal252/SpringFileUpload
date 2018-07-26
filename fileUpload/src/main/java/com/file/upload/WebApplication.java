2bbefd95957a01327a99cac01c02d2da8b49e5a9
chirag <cgoyal252@gmail.com>
2018-07-26 18:16:37 +0530

    project

---
+++ fileUpload/src/main/java/com/file/upload/WebApplication.java
@@ -0,0 +1,25 @@
+package com.file.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+
+@SpringBootApplication
+public class WebApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(WebApplication.class, args);
+    }
+    
+   
+    
+}
